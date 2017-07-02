5780b6da952aacf388b0066203b85467f56de599
suyashSax <suyash2099@gmail.com>
2017-07-02 19:25:33 +0400

    Setup distinct test and dev envs

---
+++ todo-api/server/config/config.js
@@ -0,0 +1,11 @@
+var env = process.env.NODE_ENV || 'dev'
+console.log('*** ENV ***', env)
+
+if (env === 'dev'){
+    process.env.PORT = 3000
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+}
+else if (env === 'test'){
+    process.env.PORT = 3000
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+}
