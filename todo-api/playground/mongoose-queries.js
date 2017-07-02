330a2f2e47623701c5c8e130b292c3ec002951f6
suyashSax <suyash2099@gmail.com>
2017-07-02 13:35:54 +0400

    Add queries playground

---
+++ todo-api/playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {mongoose} = require('./../server/db/mongoose')
+const {ObjectID} = require('mongodb')
+const {Todo} = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+
+var id = '5958ba58fda7de1edd4737a8'
+var uid = '5957ee4f5ed33ac8f386703e'
+
+if(!ObjectID.isValid(id)){
+    console.log('Id not valid')
+}
+
+Todo.find({                             // returns array even if just one object (i.e. inconvenient [] for empty response)
+    _id: id
+}).then((todos) => {
+    console.log('Todos: ', todos)
+})
+
+Todo.findOne({                          // returns object (i.e. convenient null for empty response)
+    _id: id
+}).then((todo) => {
+    console.log('Todo: ', todo)
+})
+
+Todo.findById(id).then((todo) => {
+    if (!todo){
+        return console.log('ID not found...')
+    }
+    console.log('Todo by ID: ', todo)
+}).catch((e) => console.log(e))
+
+User.findById(uid).then((user) => {
+    if (!user){
+        return console.log('User not found...')
+    }
+    console.log('User by ID: ', user)
+}).catch((e) => console.log(e))
