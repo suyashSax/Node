67af63ef032289b6e894dc288c8b341282ecd636
suyashSax <suyash2099@gmail.com>
2017-07-03 16:54:37 +0400

    Add POST /users route and refactor user schema

--- todo-api/server/models/user.js
+++ todo-api/server/models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
 mongoose.Promise = global.Promise
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -31,5 +33,27 @@ var User = mongoose.model('User', {
         }
     }]
 })
+
+// functions bind this, arrow funcs don't 
+
+UserSchema.methods.toJSON = function(){
+    var user = this
+    var userObj = user.toObject()
+    return _.pick(userObj, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function(){
+    var user = this
+    var access = 'auth'
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'secret').toString()
+    
+    // tokens is a normal array...
+    user.tokens.push({access, token})
+    return user.save().then(() => {
+        return token
+    })
+}
+
+var User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
